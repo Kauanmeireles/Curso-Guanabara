f5fee30e95e43b8f97053122e592eb100e19f6d9
Kauan <kauanmeireles2007@gmail.com>
2026-05-01 14:00:12 -0300

    Remove código comentado e atualiza a impressão da soma para f-string

--- aula 06.py
+++ aula 06.py
@@ -2,5 +2,4 @@ n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
 
 s = n1 + n2
- # print('a soma entre', n1, 'e', n2, 'vale' : s)
-print('A soma entre {} e {} vale {}'.format(n1,n2, s))
+print(f'A soma entre {n1} e {n2} vale {s}')
