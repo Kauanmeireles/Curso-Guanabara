acfb38b69b2d2f20f003f4a2d205412c263c5c5d
Kauan <kauanmeireles2007@gmail.com>
2026-05-01 14:33:02 -0300

    adiciona símbolo de moeda na entrada e na saída de conversão

--- desafio 9 2.py
+++ desafio 9 2.py
@@ -1,9 +1,10 @@
-n = float(input('Me Fale o valor da sua carteira: '))
+n = float(input('Me Fale o valor da sua carteira: R$ '))
 cotaçao = 4.98
+cotaçao2 = 5.85
 
 dolar = n / cotaçao 
-
-print(f'Seu valor de {n} compra {dolar:.2f}!')
+euro = n / cotaçao2
+print(f'Seu valor de {n} compra R${dolar:.2f} em dolar e  R${euro:.2f} em euro!')
 
 
 
